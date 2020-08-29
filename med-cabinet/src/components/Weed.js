05f47bf33d9ce0173015a2a1893739c508dbb033
Daniel Terry <daniel.terry2121@gmail.com>
2020-08-28 22:59:28 -0400

    Rendering components

--- med-cabinet/src/components/Weed.js
+++ med-cabinet/src/components/Weed.js
@@ -9,10 +9,10 @@ const Weed = (props) => {
             {props.weed.map(weed => (
              <Col>
                 <div key={weed.id}>
-                    <img src = {weed.url} alt = "Bud type" />
-                    <h2>{weed.name}</h2>
-                    <p>{weed.effects}</p>
-                    <p>{weed.flavors}</p>
+                  {console.log(weed.flavor[0])}
+                  {console.log(weed.strain[0])}
+                    <h2>{weed.strain[0]}</h2>
+                    <p>{weed.description[0]}</p>
                 </div>
              </Col>
             ))}
