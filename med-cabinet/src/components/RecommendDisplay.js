e55807009793f243487109523f35360776932da8
Freet8707 <freet8707@gmail.com>
2020-08-29 22:29:22 -0500

    styling added to RecommendDisplay.js component

--- med-cabinet/src/components/RecommendDisplay.js
+++ med-cabinet/src/components/RecommendDisplay.js
@@ -2,6 +2,30 @@ import React, { useState, useEffect } from 'react'
 // import { useHistory } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { deleteRecommend } from '../actions/actions'
+import styled from 'styled-components'
+
+const Recommendations = styled.div`
+    width: 800px;
+    margin: 0 auto;
+    padding: 40px 0 40px 30px;
+    border-radius: 10px;
+    background: #6ed36e;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-start;
+    div {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: flex-start;
+        span {
+            color: rgb(0, 100, 0);
+            font-weight: 800;
+        }
+    }
+`;
 
 const RecommendDisplay = (props) => {
     console.log(props.recommendSuccess)
@@ -17,26 +41,26 @@ const RecommendDisplay = (props) => {
     
     // const { description, effect, flavor, rating, strain, type } = props.recommendSuccess[0]
     return (
-        <>
-        {props.recommendRetrieve ? <p style={{color: 'red'}}>Retrieving Recommendations...</p> : null}
-        {data.length > 0 ? 
-        <div>
-            <h1>Your List of Recommendations : {console.log(data[0])}</h1>
-            <h3>Strain: {data[0].strain}</h3>
-            <h3>{data[0].type}</h3>
-            <p>description: {data[0].description}</p>
-            <p>{data[0].flavor}</p>
-            <p>effects: {data[0].effect}</p>
-            <p>rating: {data[0].rating}</p>
-            <button 
-                onClick={() => {
-                    props.deleteRecommend(data[0].id)}
-                }>
-                Delete Recommendation ?
-            </button>            
-        </div> :
-        <p style={{color: 'red'}}>Recommendation Deleted ! </p>}
-        </>
+        <Recommendations>
+            {props.recommendRetrieve ? <p style={{color: 'red'}}>Retrieving Recommendations...</p> : null}
+            {data.length > 0 ? 
+            <div>
+                <h1>Your List of Recommendations : {console.log(data[0])}</h1>
+                <h3><span>Strain: </span> {data[0].strain}</h3>
+                <h3>{data[0].type}</h3>
+                <p><span>description: </span> {data[0].description}</p>
+                <p>{data[0].flavor}</p>
+                <p><span>effects: </span> {data[0].effect}</p>
+                <p><span>rating: </span> {data[0].rating}</p>
+                <button 
+                    onClick={() => {
+                        props.deleteRecommend(data[0].id)}
+                    }>
+                    Delete Recommendation ?
+                </button>            
+            </div> :
+            <p style={{color: 'red'}}>Recommendation Deleted ! </p>}
+        </Recommendations>
     )
 }
 
